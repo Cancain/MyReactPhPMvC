70920a432a7c90229fd9328a728478dbf8c3fb95
Cancain <eriksson.tomas@gmail.com>
2019-02-26 09:19:09 +0100

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = 'http://localhost/react-mvc/Backend/libraries/bootstrap.php';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
